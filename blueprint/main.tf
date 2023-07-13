52bfc981728fec3f925037397fca86dba9b8fdbe
pratyusha-deekshit <126081699+pratyusha-deekshit@users.noreply.github.com>
2023-07-13 14:30:58 +0530

    Update main.tf

--- blueprint/main.tf
+++ blueprint/main.tf
@@ -19,7 +19,7 @@ provider "genesyscloud" {
 }
 
 resource "genesyscloud_tf_export" "export" {
-  directory          = ./genesyscloud
+  directory          = "./genesyscloud"
   export_as_hcl      = true
   resource_types     = ["genesyscloud_user"]
   include_state_file = true
