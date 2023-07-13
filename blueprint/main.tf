4a6f675acb988fdcda93c04fc4154907397d7408
pratyusha-deekshit <126081699+pratyusha-deekshit@users.noreply.github.com>
2023-07-13 16:49:12 +0530

    Update main.tf

--- blueprint/main.tf
+++ blueprint/main.tf
@@ -18,6 +18,30 @@ provider "genesyscloud" {
   sdk_debug = true
 }
 
+resource "genesyscloud_user" "demo_user1" {
+  email           = "demo.user1@abcd.com"
+  name            = "Demo User1"
+  password        = "b@Zinga1972"
+  state           = "active"
+  department      = "IRA"
+  title           = "Agent"
+  acd_auto_answer = true
+  addresses {
+
+    phone_numbers {
+      number     = "+9205551212"
+      media_type = "PHONE"
+      type       = "MOBILE"
+    }
+  }
+  employer_info {
+    official_name = "Demo User1"
+    employee_id   = "12345"
+    employee_type = "Full-time"
+    date_hire     = "2023-03-18"
+  }
+}
+
 resource "genesyscloud_tf_export" "export" {
   directory          = "./genesyscloud"
   export_as_hcl      = true
